91bfbae19beee75be8a8457b7aea24637af9e99b
andela-kanyanwu <kosy.anyanwu@andela.co>
2015-03-14 20:32:18 +0100

    Loaded map with current location using angular

--- js/controller.js
+++ js/controller.js
@@ -1,33 +1,52 @@
-var app = angular.module('BankLocator', []);
+var app = angular.module('BankLocator', ['uiGmapgoogle-maps']);
 
-app.controller('MainController', ['$scope', function($scope) {
+app.controller('MainController', ['$scope', '$window', '$log', '$timeout',function($scope, $window, $log, $timeout) {
 
-  //Loads map with user's current location
-  var map;
+  //Gets user's current location to load the map
+  window.navigator.geolocation.getCurrentPosition(function (response){
+    var lat = response.coords.latitude,
+        lng = response.coords.longitude;
 
-  var initializeMap = function() {
-    var mapOptions = {
-      zoom: 15
+    //To get the value of the latitude and longitude available anywhere in the app
+    // $rootScope.currPosition = {lat: lat, lng: lng};
+
+  //loads the map
+    $scope.map = { 
+      center: {
+        latitude: lat, 
+        longitude: lng
+      }, 
+      zoom: 16 
     };
-    map = new google.maps.Map(document.getElementById('mapArea'),
-      mapOptions);
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = new google.maps.LatLng(position.coords.latitude,
-          position.coords.longitude);
 
-        map.setCenter(pos);
-      });
+    //sets the marker
+    $scope.marker = {
+      coords: {
+        latitude: lat, 
+        longitude: lng
+      },
+      options: { 
+        draggable: true 
+      },
+      id: 0,
+      events: {
+        dragend: function (marker, eventName, args) {
+          $log.log('marker dragend');
+          var lat = marker.getPosition().lat();
+          var lon = marker.getPosition().lng();
+          // $log.log(lat);
+          // $log.log(lon);
+
+          $scope.marker.options = {
+            draggable: true,
+            // labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
+            labelAnchor: "100 0",
+            labelClass: "marker-labels"
+          };
+        }
+      }
     }
-  }
-  initializeMap()
 
-  //   // navigator.geolocation.getCurrentPosition(function success(Position){console.log(Position.coords.latitude,Position.coords.longitude );})
-
-  //   //loads the map
-  //   $scope.map = {  
-  //     center: {lat and lng}, 
-  //     zoom: 8 
-  //   };
-
+    $timeout(function(){});
+  })
 }]);
--- js/services.js
+++ js/services.js
@@ -1,12 +1,18 @@
-// var app = angular.module('BankLocator',[]);
+// var app = angular.module('BankSearch',[]);
 
-// app.factory('Locator', ['$http', function($http){
+// app.factory('locator', ['$http', '$rootScope', function($http, $rootScope){
+//   var url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
+//   var data = {
+//     location: $rootScope.currPosition.lat + ',' + $rootScope.currPosition.lng,
+//     radius: '500',
+//     types: 'bank',
+//     key: 'AIzaSyCu-MS1i1v3KAhX33xu0UdMJDHsEfsPt7o'
+//   }
+  
+//   $http.get(url, data).
+//     success(function(response){
+//       console.log(response);
+//     })
+// }])
 
-//   var url = 'https://google.com/aindioasndoiasndoas';
-//   return {
-//     get: function (cb) {
-//       return $http.get(url)
-//         .success(cb)
-//     }
-//   };
-// }]);
+// 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=6.5067666,3.3784109&radius=500&types=bank&key=AIzaSyCu-MS1i1v3KAhX33xu0UdMJDHsEfsPt7o'
