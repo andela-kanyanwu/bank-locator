880ddc71ab35907ff22167e3af274fe42d67b295
andela-kanyanwu <kosy.anyanwu@andela.co>
2015-03-17 01:19:33 +0100

    Created factory for http

--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,6 @@
-var app = angular.module('BankLocator', ['uiGmapgoogle-maps']);
+var app = angular.module('BankLocator', ['uiGmapgoogle-maps', 'BankSearch']);
 
-app.controller('MainController', ['$scope', '$window', '$log', '$timeout', '$http', function($scope, $window, $log, $timeout, $http) {
+app.controller('MainController', ['$scope', '$window', '$log', '$timeout', 'locator', function($scope, $window, $log, $timeout, locator) {
 
   //Gets user's current location to load the map
   window.navigator.geolocation.getCurrentPosition(function(response) {
@@ -21,17 +21,8 @@ app.controller('MainController', ['$scope', '$window', '$log', '$timeout', '$htt
 
     $scope.markers = createMarkers;
 
-    $http({
-      method: 'GET',
-      url: 'https://api.foursquare.com/v2/venues/search',
-      params: {
-        client_id: "BCFWS4SVPQBUKM4EEODKUYWVROZNIIVQ35ZOTAUOSL3HCDF4",
-        client_secret: "14LEMG1H3AVUFQHFTNE4DR13JNDNSJJY4TX2YRXCAUN500P5",
-        v: "20130815",
-        ll: lat + "," + lng,
-        query: "bank"
-      }
-    }).
+    //call the bank location api here
+    locator.get(lat, lng).
     success(function(reply) {
       $scope.banks = reply.response.venues;
 
@@ -39,9 +30,8 @@ app.controller('MainController', ['$scope', '$window', '$log', '$timeout', '$htt
       var markerTitle = "";
       for (var i = 0; i < len; i++) {
         if (reply.response.venues[i].location.address === undefined) {
-          markerTitle = reply.response.venues[i].name; 
-        }
-        else {
+          markerTitle = reply.response.venues[i].name;
+        } else {
           markerTitle = reply.response.venues[i].name + ", " + reply.response.venues[i].location.address;
         }
         createMarkers.push({
--- js/services.js
+++ js/services.js
@@ -1,6 +1,19 @@
-// // var app = angular.module('BankSearch',[]);
+var app = angular.module('BankSearch', []);
 
-// // app.factory('locator', ['$http', function($http){
-
-// // }])
-
+app.factory('locator', ['$http', function($http) {
+  return {
+    get: function(lat, lng) {
+      return $http({
+        method: 'GET',
+        url: 'https://api.foursquare.com/v2/venues/search',
+        params: {
+          client_id: "BCFWS4SVPQBUKM4EEODKUYWVROZNIIVQ35ZOTAUOSL3HCDF4",
+          client_secret: "14LEMG1H3AVUFQHFTNE4DR13JNDNSJJY4TX2YRXCAUN500P5",
+          v: "20130815",
+          ll: lat + "," + lng,
+          query: "bank"
+        }
+      }); 
+    }
+  }
+}])
